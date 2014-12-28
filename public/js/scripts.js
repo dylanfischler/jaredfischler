a9bc0438ce00935cbdfb5129f1e11272852bcfea
Dylan Fischler <dylanfischler@Dylans-MacBook-Pro.local>
2014-12-28 15:07:29 -0500

    Brand section added

---
+++ public/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	//responsive logo height
+
+		//set initial
+		var logoHeight = Math.floor($(window).width()/30);
+		$("section#brand .logo").css("top",(logoHeight + "%"));
+		//bind to resize event
+		$(window).resize(function(){
+			logoHeight = Math.floor($(window).width()/30);
+			$("section#brand .logo").css("top",(logoHeight + "%"))
+		});
+
+});
