92cab52de697bb7607597786f3a8a703cfe156c8
Jared Fischler <jaredfischler@Jareds-MacBook-Pro.local>
2015-01-07 18:09:31 -0500

    Adding more desc and shtuff

--- data/projects_json.js
+++ data/projects_json.js
@@ -14,50 +14,58 @@ module.exports = function(){
 			title: "Concept Designs for The Voyage",
 			id: "concept_design_for_the_voyage",
 			category: "3dmodeling",
-			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non fermentum risus, commodo aliquet orci. Maecenas in nibh laoreet, vehicula eros eu, tincidunt mi. Aenean vel convallis tortor. Nullam non eros lobortis ipsum efficitur ornare. Nulla eu ultrices libero, ac fermentum sem. Fusce sed aliquet augue, dapibus hendrerit quam. Maecenas sit amet justo quis metus pellentesque vehicula eget auctor orci. Ut ac tellus pulvinar, lobortis nisi condimentum, pharetra nulla. Pellentesque ut quam non lectus ornare blandit quis sed purus. Mauris elementum ligula turpis, ut dictum justo semper sit amet.",
-			mainType: "landscapeImage"
+			desc: "In preparation for a new chapter of The Voyage, I have started to conceptualize what creatures may populate the environments and what those environments may look like.",
+			mainType: "landscapeImage",
+			addImgs: ["ConceptAlternative1", "ConceptAlternative2", "ConceptAlternative3"]
 		},
 		{
 			title: "Concept Design: Spacecraft",
 			id: "concept_design_spacecraft",
 			category: "3dmodeling",
-			desc: "",
-			mainType: "landscapeImage"
+			desc: "A spacecraft concept for The Voyage",
+			mainType: "landscapeImage",
+			addImgs: ["SpacecraftAlternative1", "SpacecraftAlternative2"]
+
 		},
 		{
 			title: "Plate Camera",
 			id: "plate_camera",
 			category: "3dmodeling",
-			desc: "",
-			mainType: "landscapeImage"
+			desc: "A Plate Camera. Modeled, textured, and lit in Cinema 4d.",
+			mainType: "landscapeImage",
+			addImgs: ["CameraAlternative1", "CameraAlternative2", "CameraAlternative3", "CameraAlternative4"]
 		},
 		//Animation
 		{
 			title: "The Voyage | Ch.1: Voyage Begins",
 			id: "the_voyage_ch1_voyage_begins",
 			category: "animation",
-			desc: "",
-			mainType: "landscapeImage"
+			desc: "The start of a journey. Music -- Chord Left - Agnes Obel",
+			mainType: "htmlEmbed",
+			html: '<iframe width="1280" height="720" src="//www.youtube.com/embed/To8wZwBR-xg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>'
+			addImgs: ["VoyageAlternative1", "VoyageAlternative2", "VoyageAlternative3", "VoyageAlternative4", "VoyageAlternative5", "VoyageAlternative6", "VoyageAlternative7", "VoyageAlternative8", "VoyageAlternative9",
 		},
 		{
 			title: "Why Buy Local?",
 			id: "why_buy_local",
 			category: "animation",
-			desc: "",
-			mainType: "landscapeImage"
+			desc: "My first AfterEffects Animation: A motion graphic PSA about locally grown food.",
+			mainType: "htmlEmbed",
+			html: '<iframe src="//player.vimeo.com/video/106324933?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 		},
 		{
 			title: "Dream Lapse",
 			id: "dream_lapse",
 			category: "animation",
-			desc: "",
-			mainType: "landscapeImage"
+			desc: "Experimenting in After-effects with the help of Blockhead. Track: Triptych Pt.3 - Blockhead, Music by Cavelight",
+			mainType: "htmlEmbed"
+			html: '<iframe src="//player.vimeo.com/video/90440259?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 		},
 		{
 			title: "Birth - The Infant's Discovery of Self",
 			id: "birth_the_infants_discovery_of_self",
 			category: "animation",
-			desc: "",
+			desc: "A short piece exploring the process of discovery and awareness of self.",
 			mainType: "htmlEmbed",
 			html: '<iframe src="//player.vimeo.com/video/94679492?title=0&amp;byline=0&amp;portrait=0&amp;color=000" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 		},
@@ -65,30 +73,34 @@ module.exports = function(){
 			title: "The Cubicle",
 			id: "the_cubicle",
 			category: "animation",
-			desc: "",
-			mainType: "landscapeImage"
+			desc: "An animation about a man named Malcom who discovers an old favorite of his on his computer. More to come!",
+			mainType: "htmlEmbed",
+			html: '<iframe src="//player.vimeo.com/video/94576283?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 		},
 		//Cinematography
 		{
 			title: "The Study Montage",
 			id: "the_study_montage",
 			category: "cinematography",
-			desc: "",
-			mainType: "landscapeImage"
+			desc: "A video montage",
+			mainType: "htmlEmbed",
+			html: '<iframe src="//player.vimeo.com/video/108959227?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 		},
 		{
 			title: "The Forgotten Keys",
 			id: "the_forgotten_keys",
 			category: "cinematography",
-			desc: "",
-			mainType: "landscapeImage"
+			desc: "A story about a forgotten pair of keys",
+			mainType: "htmlEmbed",
+			html: '<iframe src="//player.vimeo.com/video/108353542?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 		},
 		{
 			title: "The Date",
 			id: "the_date",
 			category: "cinematography",
-			desc: "",
-			mainType: "landscapeImage"
+			desc: "An Experiment in continuity editing",
+			mainType: "htmlEmbed",
+			html: '<iframe src="//player.vimeo.com/video/108298047?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 		},
 		//Digital Art
 		{
