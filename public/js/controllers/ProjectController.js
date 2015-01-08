51094088c1efa995d8252c4c541d6bfa4ee7cc7a
Dylan Fischler <dylanfischler@Dylans-MacBook-Pro.local>
2015-01-08 12:11:36 -0500

    Title and description on project pages moved above images. Support for additional images and html embeds added to project pages. landscapeImage max size now based on screen width.

--- public/js/controllers/ProjectController.js
+++ public/js/controllers/ProjectController.js
@@ -7,9 +7,11 @@ app.controller("ProjectController", ['$scope', '$http', '$routeParams', '$sce',
 			params: {id: $scope.projectID}
 		});
 		res.success(function(data, status, headers, config) {
-			console.log(data);
 			$scope.project = data;
 			$scope.project.html = $sce.trustAsHtml($scope.project.html);
+			for(var i = 0; i < $scope.project.addHtml.length; i++){
+				$scope.project.addHtml[i] = $sce.trustAsHtml($scope.project.addHtml[i]);
+			}
 	    });
 	    res.error(function(data, status, headers, config) {
 	        console.log("Project Retrieval Failure");
